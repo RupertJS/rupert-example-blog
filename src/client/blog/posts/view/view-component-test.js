e41f0a6a9bd7f2cb757d1b4912ae8348c3c8bf47
David Souther <davidsouther@gmail.com>
2015-06-21 12:48:55 -0400

    Factored some example code.

--- src/client/blog/posts/view/view-component-test.js
+++ src/client/blog/posts/view/view-component-test.js
@@ -7,7 +7,7 @@ describe('Post View Component', function(){
       content: '# Post'
     };
     $httpBackend.expectGET('/api/v1/posts/123').respond(samplePost);
-    var postView = renderRoute('/posts/view/123');
+    /*var postView = */renderRoute('/posts/view/123');
     $route.current.params.should.have.property('postId').that.equals('123');
     // TODO more assertions
   }));
--- src/client/blog/renderer/renderer-service-test.js
+++ src/client/blog/renderer/renderer-service-test.js
@@ -1,28 +1,30 @@
-describe('Example Blog', function(){
-  describe('Renderer Service', function(){
-    beforeEach(module('renderer'));
+describe('Renderer Service', function(){
+  beforeEach(module('renderer'));
 
-    var _oldMarked = null;
-    beforeEach(inject(function($q){
-      _oldMarked = marked;
-      marked = sinon.stub().returns($q.when('<h1>Hello</h1>'));
-      marked.setOptions = sinon.spy();
-    }));
-    afterEach(function(){
-      marked = _oldMarked;
-      _oldMarked = null;
+  stubOutMarked();
+
+  it('returns a markdown renderer', inject(function(renderer, $rootScope){
+    var source = '# Hello';
+    var expected = '<h1>Hello</h1>';
+    var actual = null;
+    renderer(source).then(function(result){
+      actual = result;
     });
-
-    it('returns a markdown renderer', inject(function(renderer, $rootScope){
-      var source = '# Hello';
-      var expected = '<h1>Hello</h1>';
-      var actual = null;
-      renderer(source).then(function(result){
-        actual = result;
-      });
-      $rootScope.$digest();
-      marked.should.have.been.calledWith(source);
-      actual.should.equal(expected);
-    }));
-  });
+    $rootScope.$digest();
+    marked.should.have.been.calledWith(source);
+    actual.should.equal(expected);
+  }));
 });
+
+function stubOutMarked(){
+  var _oldMarked = null;
+  beforeEach(inject(function($q){
+    _oldMarked = marked;
+    marked = sinon.stub().returns($q.when('<h1>Hello</h1>'));
+    marked.setOptions = sinon.spy();
+  }));
+  afterEach(function(){
+    marked = _oldMarked;
+    _oldMarked = null;
+  });
+}
